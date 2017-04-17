fc4faf3239173e5aa7f32a3165ecf2b317902771
CodeDraken <CodeDraken@gmail.com>
2017-04-17 14:13:04 -0400

    Add mongo process.env

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://test:test153@ds163010.mlab.com:63010/mongo-todo');
-//mongoose.connect('mongodb://localhost:27017/TodoApp');
+//mongoose.connect('mongodb://test:test153@ds163010.mlab.com:63010/mongo-todo');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
