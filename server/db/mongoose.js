db6face9fd4c984de92411cb8189caaca50cfcd1
CodeDraken <codedraken@gmail.com>
2017-04-19 11:06:30 -0400

    Separate test database

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 let mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-//mongoose.connect('mongodb://test:test153@ds163010.mlab.com:63010/mongo-todo');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
